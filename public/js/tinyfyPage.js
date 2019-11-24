7f16e3ea51cb1d8feb16e480085c24d73f756ae6
Anders Nygren <anders.nygren@tretton37.com>
2019-11-24 14:22:44 +0100

    Basic copy-button feedback

--- public/js/tinyfyPage.js
+++ public/js/tinyfyPage.js
@@ -5,19 +5,29 @@
   copyButtons.forEach(button => button.addEventListener('click', copyTinyUrl))
 
   async function copyTinyUrl(event) {
-    const tinyUrl = event.target.dataset.tinyUrl
+    const button = event.target
+    const tinyUrl = button.dataset.tinyUrl
     const textInput = document.createElement('input')
+
     textInput.classList.add('copy-input')
     textInput.type = 'text'
     textInput.value = tinyUrl
     document.body.appendChild(textInput)
     textInput.select()
     document.execCommand('copy')
+
     const successful = document.execCommand('copy')
-
-    const msg = successful ? 'successful' : 'unsuccessful'
-    console.log('Copying text command was ' + msg)
-
     document.body.removeChild(textInput)
+
+    const feedback = successful ? 'Copied!' : 'Failed :('
+    const feedbackClass = successful ? 'copy-success' : 'copy-fail'
+
+    const originalButtonText = button.innerText
+    button.classList.add(feedbackClass)
+    button.innerText = feedback
+    setTimeout(() => {
+      button.classList.remove(feedbackClass)
+      button.innerText = originalButtonText
+    }, 1000)
   }
 })()
