c272155ef847435c2faeef2bb72a8e78e576966f
Anders Nygren <anders.nygren@tretton37.com>
2019-11-24 14:22:44 +0100

    More integration tests

--- test/functional/main.spec.js
+++ test/functional/main.spec.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const clipboardy = require('clipboardy')
 const { test, trait } = use('Test/Suite')('Main')
 
 trait('Test/Browser')
+
+trait('DatabaseTransactions')
 
 test('Should show main page', async ({ browser }) => {
   const page = await browser.visit('/')
@@ -12,8 +15,8 @@ test('Should show main page', async ({ browser }) => {
 test('Should be able to shorten an url, redirect back and show tinyfied url', async ({
   browser,
 }) => {
-  const testUrl = 'http://testurl.com'
   const page = await browser.visit('/')
+  const testUrl = 'http://testurl.com'
 
   await page
     .type('[name="url"]', testUrl)
@@ -21,4 +24,54 @@ test('Should be able to shorten an url, redirect back and show tinyfied url', as
     .waitForNavigation()
     .assertPath('/')
     .assertHas(testUrl)
+})
+
+test('User should see 10 latest urls', async ({ browser }) => {
+  const page = await browser.visit('/')
+  const urls = [
+    'first.se',
+    'second.se',
+    'third.se',
+    'fourth.se',
+    'fifth.se',
+    'sixth.se',
+    'seventh.se',
+    'eights.se',
+    'ninth.se',
+    'tenth.se',
+    'eleventh.se',
+  ]
+
+  for (const url of urls) {
+    await page
+      .type('[name="url"]', url)
+      .submitForm('form')
+      .waitForNavigation()
+      .assertPath('/')
+  }
+
+  for (const url of urls.slice(1)) {
+    await page.assertHas(url)
+  }
+})
+
+test('User should be able to copy the shortened url to clipboard with copy button', async ({
+  assert,
+  browser,
+}) => {
+  const page = await browser.visit('/')
+  const testUrl = 'https://testurl.com'
+
+  await page
+    .type('[name="url"]', testUrl)
+    .submitForm('form')
+    .waitForNavigation()
+    .click('button.copy-button')
+
+  const tinyTestUrl = await page.getAttribute(
+    'button.copy-button',
+    'data-tiny-url'
+  )
+
+  assert.equal(clipboardy.readSync(), tinyTestUrl)
 })
